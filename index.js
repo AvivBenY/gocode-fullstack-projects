33236393d12168f25895785158bbf2f2e29c01c8
AvivBenY <benyedidia123@gmail.com>
2022-05-25 14:43:41 +0300

    class project

---
+++ index.js
@@ -0,0 +1,35 @@
+const input = document.querySelector(".searched");
+const ulEl = document.querySelector(".searchList")
+
+const defaultArr = ["banana","apple","potato"]
+
+displayList(defaultArr);
+input.addEventListener('input', function (e) {
+    isInclude(defaultArr, e.target.value);
+});
+function displayList(arr) {
+    ulEl.innerHTML="";
+    if (arr.length === 0) {
+        const liEl = document.createElement("li");
+        liEl.classList.add("not-found");
+        liEl.textContent = "Not results found!";
+        ulEl.append(liEl);
+    }
+    for (let i = 0; i < arr.length; i++) {
+        const liEl = document.createElement("li");
+        liEl.textContent = arr[i];
+        ulEl.append(liEl);
+    }
+}
+
+function isInclude(arr, str) {
+    if(str ===""){
+        displayList(defaultArr);
+    }
+    else{
+        const filteredArr = arr.filter(function (word) {
+        return word.startsWith(str);
+    });
+    console.log(str);
+    displayList(filteredArr);
+}}
